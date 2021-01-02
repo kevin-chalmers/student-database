8fa3b97142d102c0fd7d2f9f8ee5ef6da171c638
Kevin Chalmers <kevin.chalmers@roehampton.ac.uk>
2021-01-02 15:58:01 +0000

    Starting code for student database added.

---
+++ data/data.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Import SQLite library.
+// Use verbose mode to give more detailed error outputs
+const sqlite3 = require("sqlite3").verbose();
+
+// Connect to the database.
+// Function is callback when connection completed.
+// err is any error message that occurs
+let db = new sqlite3.Database("students.db", function(err) {
+    // If an error, print it out.
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Connected to students database.");
+});
---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// Include the app.js and data.js files.
+// This will run the code.
+const app = require("./application/app");
+const data = require("./data/data.js");
