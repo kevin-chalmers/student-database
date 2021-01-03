cc96e73bb5b2d1e0915eac60fd0caa4703529a96
Kevin Chalmers <kevin.chalmers@roehampton.ac.uk>
2021-01-03 17:49:24 +0000

    Added button click to Angular.js application.

--- static/index.js
+++ static/index.js
@@ -1,6 +1,30 @@
 "use strict";
 
-function printStudents() {
-    var main = document.getElementById("main");
-    main.innerHTML = "Hello, world!";
+// Definition of Student type
+class Student {
+    // ID of the student
+    id;
+    // Name of the student
+    name;
+
+    // Creates a new Student object
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
 }
+
+// Get's the mainApp section of the HTML document
+var mainApp = angular.module("mainApp", []);
+
+// Creates the controller for the main application.
+mainApp.controller("studentController", function($scope) {
+  	// Sets a new model value -- student.
+    $scope.student = new Student("001", "Kevin Chalmers");
+    // Sets the update() function for the scope. Used by the button.
+    $scope.update = function() {
+        // Update the student value in the scope
+        $scope.student.id = "002";
+        $scope.student.name = "Lisa Haskel";
+    }
+});
